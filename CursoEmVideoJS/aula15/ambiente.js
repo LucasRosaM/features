2286e962e90bb7af84626581aa1c29b5b8597d9e
Lucas Rosa <Lucasmioransa2000@hotmail.com>
2020-12-20 11:52:50 -0300

    Curso em video JavaScript Exercicios

---
+++ CursoEmVideoJS/aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 2, 9, 3]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posicoes`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos = num.indexOf(8)
+console.log(`O valor 8 esta na posicao ${pos}`)
